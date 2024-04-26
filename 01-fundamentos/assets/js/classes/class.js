5f8ccf8c852eca0cc1d0a3a407353a0adf81ada3
Esteban Portillo <esteban.07.elias@gmail.com>
2024-04-25 20:06:30 -0600

    Sets y Gets

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,7 +1,9 @@
 class Persona {
+  // Propiedades y Métodos Estáticos
   nombre = '';
   codigo = '';
   frase = '';
+  comida = '';
 
   // El constructor es un método que se va a ejecutar
   // en el momento que se crea una nueva instancia de Persona
@@ -15,6 +17,19 @@ class Persona {
     this.frase = frase;
   }
 
+  // Sets y Gets
+  // Usualmente el  'set' recibe un único argumento
+  // que es el que utilizaremos para establecerlo
+  set setComidaFavorita(comida) {
+    this.comida = comida.toUpperCase();
+  }
+
+  // El 'get' es para recuperar un valor
+  get getComidaFavorita() {
+    return `La comida favorita de ${this.nombre} es ${this.comida}`;
+  }
+
+  // Métodos
   quienSoy() {
     console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
   }
@@ -32,9 +47,13 @@ const spiderman = new Persona(
 );
 
 const ironman = new Persona('Tony Stark', 'Ironmam', 'Yo soy Ironman');
+// console.log(ironman);
+
+spiderman.miFrase();
+// ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+// spiderman.comida = 'Duende Verde'; // Esto es algo que puede suceder y se puede evitar con las propiedades privadas
 
 console.log(spiderman);
-console.log(ironman);
-
-spiderman.quienSoy();
-ironman.quienSoy();
+console.log(spiderman.getComidaFavorita);
