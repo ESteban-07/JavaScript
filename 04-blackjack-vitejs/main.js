cb5725e527406107ba3a4a9e0905760ac40d449a
Esteban Portillo <83608658+ESteban-07@users.noreply.github.com>
2024-05-02 16:17:05 +0000

    88. Trabajar con Vite - Blackjack

--- 04-blackjack-vitejs/main.js
+++ 04-blackjack-vitejs/main.js
@@ -1,24 +1,173 @@
+import _ from "underscore";
+
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello World!!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+/**
+ * Patrón Módulo
+ * - - - - - - -
+ *
+ * Las funciones anónimas autoinvocadas no tienen una referencia por nombre,
+ * lo que crea un nuevo ámbito donde no se puede acceder al objeto directamente.
+ */
 
-setupCounter(document.querySelector("#counter"));
+  let deck = [];
+  const types = ['C', 'D', 'H', 'S'],
+    specials = ['A', 'J', 'Q', 'K'];
+
+  let puntosJugadores = [];
+  let puntosJugador, puntosComputadora;
+
+  // Referencias del HTML
+  const btnNuevo = document.querySelector('#btnNuevo'),
+    btnPedir = document.querySelector('#btnPedir'),
+    btnDetener = document.querySelector('#btnDetener');
+
+  const puntosHTML = document.querySelectorAll('small');
+  const divCartasJugadores = document.querySelectorAll('.divCartas');
+
+  const inicializarJuego = (numjugadores = 2) => {
+    deck = crearDeck();
+
+    puntosJugadores = [];
+    puntosJugador = 0;
+    puntosComputadora = 0;
+
+    for (let i = 0; i < numjugadores; i++) {
+      puntosJugadores.push(0);
+    }
+
+    puntosHTML.forEach((elem) => (elem.innerText = 0));
+    divCartasJugadores.forEach((elem) => (elem.innerHTML = ''));
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+  };
+
+  // Esta función crea una nueva baraja
+  const crearDeck = () => {
+    deck = [];
+
+    // 2C, 2D, 2H, 2S
+    for (let i = 2; i <= 10; i++) {
+      for (let type of types) {
+        deck.push(`${i}${type}`);
+      }
+    }
+
+    for (let special of specials) {
+      for (let type of types) {
+        deck.push(`${special}${type}`);
+      }
+    }
+
+    return _.shuffle(deck);
+  };
+
+  // Esta función me permite tomar una carta
+  const pedirCarta = () => {
+    if (deck.length === 0) {
+      throw 'No hay cartas en el deck';
+    }
+
+    return deck.pop();
+  };
+
+  const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+
+    return isNaN(valor) ? (valor === 'A' ? 11 : 10) : valor * 1;
+  };
+
+  // Eventos
+  btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+    const turno = acumularPuntos(carta, 0);
+
+    [puntosJugador] = puntosJugadores;
+
+    insertarCarta(carta, turno);
+
+    if (puntosJugador >= 21) {
+      turnoComputadora(puntosJugador);
+    }
+  });
+
+  // Turno: 0 = primer jugador y el último será la computadora
+  const acumularPuntos = (carta, turno) => {
+    puntosJugadores[turno] += valorCarta(carta);
+    puntosHTML[turno].innerText = puntosJugadores[turno];
+
+    return turno;
+  };
+
+  const insertarCarta = (carta, turno) => {
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `./assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+
+    // Turno: 0 = primer jugador y el último será la computadora
+    divCartasJugadores[turno].append(imgCarta);
+  };
+
+  btnNuevo.addEventListener('click', () => {
+    inicializarJuego();
+  });
+
+  // turno de la computadora
+  const turnoComputadora = (puntosMinimos) => {
+    let carta, turno;
+
+    do {
+      carta = pedirCarta();
+
+      turno = acumularPuntos(carta, puntosJugadores.length - 1);
+
+      [, puntosComputadora] = puntosJugadores;
+
+      insertarCarta(carta, turno);
+
+      if (puntosMinimos >= 21) break;
+    } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+
+    deshabilitarBotones();
+  };
+
+  // Detener Juego
+  btnDetener.addEventListener('click', () => {
+    if (!puntosJugador) return;
+
+    turnoComputadora(puntosJugador);
+  });
+
+  const deshabilitarBotones = () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    imprimirGanador();
+  };
+
+  const imprimirGanador = () => {
+    if (puntosJugador <= 21) {
+      if (puntosComputadora > 21 || puntosJugador > puntosComputadora) {
+        desplegarAlerta({ title: 'Ganaste!', icon: 'success' });
+      } else if (puntosJugador === puntosComputadora) {
+        desplegarAlerta({ title: 'Empate!', icon: 'warning' });
+      } else {
+        desplegarAlerta({ title: 'Perdiste, PC gana!', icon: 'error' });
+      }
+    } else {
+      desplegarAlerta({ title: 'Perdiste, PC gana!', icon: 'error' });
+    }
+  };
+
+  const desplegarAlerta = ({ title, icon }) => {
+    return setTimeout(() => {
+      Swal.fire({
+        title,
+        icon,
+        text: `Tú: ${puntosJugador} - PC: ${puntosComputadora}`,
+      });
+    }, 1000);
+  };
+
+
