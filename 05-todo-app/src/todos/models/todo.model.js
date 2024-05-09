bf1bc119a99414c83c824f82fb94f8586e008123
Esteban Portillo <esteban.07.elias@gmail.com>
2024-05-09 13:11:00 -0600

    Tarea: creando identificador único

--- 05-todo-app/src/todos/models/todo.model.js
+++ 05-todo-app/src/todos/models/todo.model.js
@@ -1,14 +1,15 @@
 export class Todo {
-    
+
+    static counter = 0;
+
     /**
      * 
      * @param { String } description 
      */
     constructor( description ) {
-        this.id = 1;
+        this.id = Todo.counter++;
         this.description = description;
         this.done = false;
         this.createdAt = new Date();
     }
-
 }
